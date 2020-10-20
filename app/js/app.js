510942e8694407b7d249d35671bafbc508911a23
aitutu <aituufs@gmail.com>
2020-10-20 02:37:06 +0200

    first

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Custom JS
 
-	// Custom JS
+    const toggleBtn = document.querySelector('.js-toggle-mobile');
+    const header = document.querySelector('.js-header-main');
+    const nav = document.querySelector('.js-scroll');
 
+    toggleBtn.addEventListener('click', function () {
+        toggleBtn.classList.toggle('open');
+        header.classList.toggle('mobile-open');
+    });
+
+    window.onscroll = function () {
+        'use strict';
+        if (
+            document.body.scrollTop >= 44 ||
+            document.documentElement.scrollTop >= 44
+        ) {
+            nav.classList.add('scroll');
+        } else {
+            nav.classList.remove('scroll');
+        }
+    };
 });
