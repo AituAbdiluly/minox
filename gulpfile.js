4e9d8e0ce03374d016ff9c18d0da6fc522662234
Aituar <aituufs@gmail.com>
2020-11-01 02:38:14 +0100

    local lazyload

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ let paths = {
     scripts: {
         src: [
             // 'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+            baseDir + '/js/lazy.min.js',
             baseDir + '/js/app.js', // app.js. Always at the end
         ],
         dest: baseDir + '/js',
